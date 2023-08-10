d34521fd6346b8f4dc91a1f973b92c43d316773c
Quang Hưng <hung12c6thpt@gmail.com>
2023-08-10 09:48:28 +0700

    fix_function_getProducByCatId

--- getProducByCatId.js
+++ getProducByCatId.js
@@ -23,29 +23,29 @@ function getProducByCatId(req,res){
     });
 }
 
-//call api Chủ đề
-// function getProducByChuDe(req,res){
-//     var ChuDe = req.body.ChuDe;
-//     sql.executeSQL(`select * from Sach where ChuDe= N'${ChuDe}' order by productIndex`, (recordset) => {
-//         var result = "";
-//         if (recordset.recordsets[0] === null || recordset.recordsets[0].length === 0) {
-//             res.send("Chưa có sản phẩm");
-//         }
-//         else {
-//             recordset.recordsets[0].forEach(row => {
-//                 result += `
-//                 <ul style='display:inline;width:500px; height: 600px;float: left;background-color:white;color:black;text-align: center;margin-bottom: 20px;
-//                 line-height: 40px;'>
-//                 <li><a href="/detail/${row['MaSach']}"><img style="width:350px;" src='/images/${row['SachImage']}'/></a></li>
-//                 <li style="word-wrap: break-word; width:300px;margin-left: 100px;"><b>${row['TenSach']}</b></li>
-//                 <li><span style="color:red;font-size: 20px;"> ${row['DonGia']} Đ</span></li>
-//                 </ul>
-//                 `;
-//             });
-//             res.send(result);
-//         }
-//     });
-// }
+
+function getProducByChuDe(req,res){
+    var ChuDe = req.body.ChuDe;
+    sql.executeSQL(`select * from Sach where ChuDe= N'${ChuDe}' order by productIndex`, (recordset) => {
+        var result = "";
+        if (recordset.recordsets[0] === null || recordset.recordsets[0].length === 0) {
+            res.send("Chưa có sản phẩm");
+        }
+        else {
+            recordset.recordsets[0].forEach(row => {
+                result += `
+                <ul style='display:inline;width:500px; height: 600px;float: left;background-color:white;color:black;text-align: center;margin-bottom: 20px;
+                line-height: 40px;'>
+                <li><a href="/detail/${row['MaSach']}"><img style="width:350px;" src='/images/${row['SachImage']}'/></a></li>
+                <li style="word-wrap: break-word; width:300px;margin-left: 100px;"><b>${row['TenSach']}</b></li>
+                <li><span style="color:red;font-size: 20px;"> ${row['DonGia']} Đ</span></li>
+                </ul>
+                `;
+            });
+            res.send(result);
+        }
+    });
+}
 
 module.exports = {
     getProducByCatId: getProducByCatId,
